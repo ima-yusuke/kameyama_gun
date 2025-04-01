024f2f48a884ac6d0e91813f85acd0e76927cf18
ima-yusuke <yusuke1125aadxyz@gmail.com>
2025-04-01 22:36:08 +0900

    update：フロントのメニューにもfont-awesomeアイコンの追加。

--- resources/views/components/nav-component.blade.php
+++ resources/views/components/nav-component.blade.php
@@ -1,16 +1,24 @@
 <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-tab" data-tabs-toggle="#default-tab-content" role="tablist">
         <li class="me-2" role="presentation">
-            <button class="inline-block p-4 px-10 border-b-2 rounded-t-lg" id="gun-tab" data-tabs-target="#gun" type="button" role="tab" aria-controls="gun" aria-selected="false">銃</button>
+            <button class="inline-block p-4 px-10 border-b-2 rounded-t-lg" id="gun-tab" data-tabs-target="#gun" type="button" role="tab" aria-controls="gun" aria-selected="false">
+                <i class="fa-solid fa-gun pr-2"></i>銃
+            </button>
         </li>
         <li class="me-2" role="presentation">
-            <button class="inline-block p-4 px-10 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="bullet-tab" data-tabs-target="#bullet" type="button" role="tab" aria-controls="bullet" aria-selected="false">弾</button>
+            <button class="inline-block p-4 px-10 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="bullet-tab" data-tabs-target="#bullet" type="button" role="tab" aria-controls="bullet" aria-selected="false">
+                <i class="fa-solid fa-circle pr-2"></i>弾
+            </button>
         </li>
         <li class="me-2" role="presentation">
-            <button class="inline-block p-4 px-10 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="others-tab" data-tabs-target="#others" type="button" role="tab" aria-controls="others" aria-selected="false">その他</button>
+            <button class="inline-block p-4 px-10 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="others-tab" data-tabs-target="#others" type="button" role="tab" aria-controls="others" aria-selected="false">
+                <i class="fa-solid fa-briefcase pr-2"></i>その他
+            </button>
         </li>
         <li>
-            <a href="/dashboard" class="inline-block p-4 px-10 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="admin-tab" data-tabs-target="#admin" type="button" >管理画面</a>
+            <a href="/dashboard" class="inline-block p-4 px-10 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300" id="admin-tab" data-tabs-target="#admin" role="tab">
+                <i class="fa-solid fa-gear pr-2"></i>管理画面
+            </a>
         </li>
     </ul>
 </div>
